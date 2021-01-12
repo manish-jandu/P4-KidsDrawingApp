86c50a28058c3f7e0ca1392a5aaa3d160610687c
Manish <manishjandu1@gmail.com>
2021-01-12 11:23:27 +0530

    drawing remains persistene

--- app/src/main/java/com/example/p4_kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/p4_kidsdrawingapp/DrawingView.kt
@@ -14,6 +14,7 @@ class DrawingView(context: Context, attrs:AttributeSet):View(context,attrs) {
     private var mBrushSize:Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -40,6 +41,13 @@ class DrawingView(context: Context, attrs:AttributeSet):View(context,attrs) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
         canvas.drawPath(mDrawPath!!,mDrawPaint!! )
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path,mDrawPaint!!)
+        }
+
 
         if(!mDrawPath!!.isEmpty){
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
@@ -71,6 +79,7 @@ class DrawingView(context: Context, attrs:AttributeSet):View(context,attrs) {
                 }
             }
             MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color,mBrushSize)
             }
             else -> return false
@@ -81,7 +90,6 @@ class DrawingView(context: Context, attrs:AttributeSet):View(context,attrs) {
 
     internal inner class CustomPath(var color:Int,
                                     var brushThickness:Float): Path() {
-
     }
 
 }
